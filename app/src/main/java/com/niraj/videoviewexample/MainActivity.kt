a83ec0dcc0a7ca3c8b11f6f7d0d96caf9e10cee3
Niraj Gandha <nirajgandha23@gmail.com>
2022-02-15 23:20:21 +0530

    Video view basic example added
    Signed-off-by: Niraj Gandha <nirajgandha23@gmail.com>

--- app/src/main/java/com/niraj/videoviewexample/MainActivity.kt
+++ app/src/main/java/com/niraj/videoviewexample/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.niraj.videoviewexample
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import com.niraj.videoviewexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var mediaController : MediaController ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toggleButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                if (null == mediaController) {
+                    mediaController = MediaController(this)
+                }
+                mediaController?.setAnchorView(binding.videoView)
+                binding.videoView.setMediaController(mediaController)
+                binding.videoView.setVideoURI(Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"))
+                binding.videoView.start()
+            } else {
+                if (null == mediaController) {
+                    mediaController = MediaController(this)
+                }
+                mediaController?.setAnchorView(binding.videoView)
+                binding.videoView.setMediaController(mediaController)
+                binding.videoView.setVideoURI(Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"))
+                binding.videoView.start()
+            }
+        }
     }
 }
